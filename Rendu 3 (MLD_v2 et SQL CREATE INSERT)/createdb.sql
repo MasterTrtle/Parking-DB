22dd52ffd64f54eeadff485b3e0400b75bc8939a
florestan <florestan.biaux@etu.utc.fr>
2021-11-15 13:35:04 +0100

    Correctif héritage

--- Rendu 3 (MLD_v2 et SQL CREATE INSERT)/createdb.sql
+++ Rendu 3 (MLD_v2 et SQL CREATE INSERT)/createdb.sql
@@ -89,7 +89,7 @@ CREATE TABLE Employe(
 CREATE TABLE Occasionnel(
     id_client int primary key,
     foreign key (id_client) references client(id_client) on delete cascade
-) inherits (client);
+)
 
 CREATE TABLE Abonne(
     id_client int primary key,
