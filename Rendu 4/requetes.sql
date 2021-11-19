7ac10aa231f41cc21018464e6a4df37f239807bd
Alexandre Laval <alexandre.laval@etu.utc.fr>
2021-11-19 18:52:40 +0000

    Update requetes.sql

--- Rendu 4/requetes.sql
+++ Rendu 4/requetes.sql
@@ -1,11 +1,17 @@
--- on on récupère l'ensemble des reservation (idPlace, idParking, debut, fin seront placés en paramètre)
-SELECT r.debut, r.fin FROM Reservation r, place p ON r.numero_place=p.numero AND  r.parking_place = p.id_parking WHERE p.id_parking = idParking AND p.numero = idPlace;
+-- on on récupère l'ensemble des reservation d'une place  (idPlace, idParking, debut, fin seront placés en paramètre)
+SELECT r.debut, r.fin FROM Reservation r JOIN place p ON r.numero_place=p.numero AND  r.parking_place = p.id_parking WHERE p.id_parking = idParking AND p.numero = idPlace;
+
+--récuperer l'ensemble des places d'un type précis dans un parking cible
+SELECT p.idplace FROM Place a JOIN parking b ON a.id_parkiNG = b.id_parking AND p.type_vehicule ='cible';
 
 -- on effectue une réservation (debut, fin, idvehicule, client, idParking,zone, idPlace seront passés en paramètre)
 INSERT into Reservation values (debut, fin, idvehicule, client, idParking, zone, idPlace);
 
 -- récupérer l'ensemble des véhicules d'un client
 SELECT v.immat v.type_vehicule FROM Vehicule v JOIN Client c ON v.propietaire = c.id_client;
+
+--selectioner un parking en fonction de son nom
+SELECT * FROM PARKING WHERE nom = "cible";
 
 -- consulter son solde de fidélité (idabonne sera passé en paramètre)
 SELECT points_fidelite FROM Abonne WHERE id_client = idabonne;
