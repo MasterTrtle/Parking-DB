10e3f79206de95e7ad54db7788676066dc3485c0
Alexandre Laval <alexandre.laval@etu.utc.fr>
2021-11-19 18:51:29 +0000

    Update reservation.py

--- Rendu 4/reservation.py
+++ Rendu 4/reservation.py
@@ -1,40 +1,62 @@
-from datetime import datetime
-import connect
-
-
-
-def est_Reserve(idPlace, idParking, debut, fin):
-    #on on récupère l'ensemble des reservation
-    sql = "SELECT r.debut, r.fin FROM Reservation r, place p ON r.numero_place=p.numero AND  r.parking_place = p.id_parking WHERE p.id_parking = %d AND p.numero = %d "%idParking,idPlace 
+def est_Reserve(cur,idPlace, idParking, debut, fin,type):
+    #on on récupère l'ensemble des reservations
+    sql = "SELECT r.debut, r.fin FROM Reservation r, place p ON r.numero_place=p.numero AND  r.parking_place = p.id_parking WHERE p.id_parking = %d AND p.numero = %d AND p.type_vehicule =%s "%idParking,idPlace,type
     #a voir si on trie pour optimiser
-    conn = connect.get_connection()
-    allReservation = conn.cursor()
-    allReservation.execute(sql)
-    reservation = allReservation.fetchone()
+    cur.execute(sql)
+    reservation = cur.fetchone()
     flag = False
-
     while reservation:
         if (not (reservation[1]<=debut or fin<= reservation[0])):
             flag = True
             break
-        reservation = allReservation.fetchone()
-
+        reservation = cur.fetchone()
     return flag;
 
+def selectionnerVehicule(cur):
+    sql = "SELECT v.immat v.type_vehicule FROM Vehicule v JOIN Client c ON v.propietaire = c.id_client;"
+    cur.execute(sql)
+    vehicule=cur.fetchone()
+    i=0
+    while vehicule:
+        print("pour réserver une place pour le véhicule entrez",i)
+        i+=1
+        vehicule = cur.fetchone()
+    choix = input()
+    cur.execute(sql)
+    vehicule=cur.fetchone()
+    i=0
+    while i<choix:
+        i+=1
+        vehicule = cur.fetchone()
+    return vehicule
 
+def selectionnerParking(cur):
+    nom_parking =input("entrez le nom du parking ou vous souhaiter réserver une place")
+    sql = "SELECT idParking, zone FROM PARKING WHERE nom = %s;"%nom_parking
+    cur.execute(sql)
+    return cur.fetchone()
 
-def reserverPlace(debut, fin, idvehicule, client, idParking,zone, idPlace):
-
+def reserver_place_cible(cur,debut, fin, idvehicule, client, idParking,zone, idPlace):
     if est_Reserve(idPlace, idParking,debut,fin):
         print("reservation impossible, la place est déjà réservée")
     else:
-        debut = debut.strftime('%Y-%m-%d %H:%M:%S')
-        fin = fin.strftime('%Y-%m-%d %H:%M:%S')
         sql = "INSERT into Reservation values ('%s','%s','%s','%s','%s','%s','%s')", (debut, fin, idvehicule, client, idParking,zone, idPlace)
-        conn = connect.get_connection()
-        cur = conn.cursor()
         cur.execute(sql)
 
-
-
-def nombreTotalPlaceDispo(date):
+def reserver_place(cur,client):
+    vehicule = selectionnerVehicule(cur)
+    idvehicule = vehicule[0]
+    typevehicule = vehicule[1]
+    debut = input("entrez debut")
+    fin = input("entrez fin")
+    debut = debut.strftime('%Y-%m-%d %H:%M:%S')
+    fin = fin.strftime('%Y-%m-%d %H:%M:%S')
+    parking = selectionnerParking(cur)
+    idParking = parking[0]
+    zone = parking[1]
+    sql ="SELECT p.idplace FROM Place a JOIN parking b ON a.id_parkiNG = b.id_parking AND p.type_vehicule ='cible';"
+    cur.execute(sql)
+    idPlace=cur.fetchone()
+    while not est_Reserve(cur,idPlace, idParking, debut, fin,typevehicule):
+        idPlace = cur.fetchone()
+    reserver_place_cible(cur,debut, fin, idvehicule, client, idParking,zone, idPlace)
