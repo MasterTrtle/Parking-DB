746fa2e166e482829b73b594a0467db6676b99a8
florestan <florestan.biaux@etu.utc.fr>
2021-11-19 18:01:49 +0100

    Connexion fonctionnelle avec empechement d'erreur en cas de login inconnu / éronné et de mdp éronné

--- Rendu 4/main.py
+++ Rendu 4/main.py
@@ -15,25 +15,35 @@ if __name__ == '__main__':
     print("   2. Se créer un compte")
     choix1 = int(input())
     if choix1 == 1:
-        login = input("Quel est votre login ?")
-        password = input("Quel est votre mot de passe ?")
-        sql = "select mdp,employe from compte where login='%s'" % login
-        cur.execute(sql)
-        raw = cur.fetchone()
-        if raw[0]==password:
-            if raw[1]:
-                employe = 1
+        retour = "false"
+        while retour == "false":
+            login = input("Quel est votre login ?")
+            sql = "select login from compte"
+            cur.execute(sql)
+            raw = cur.fetchone()
+            login_valide = 0
+            while raw:
+                if raw[0] == login:
+                    login_valide = 1
+                raw = cur.fetchone()
+            if login_valide:
+                password = input("Quel est votre mot de passe ?")
+                sql = "select mdp,employe from compte where login='%s'" % login
+                cur.execute(sql)
+                raw = cur.fetchone()
+                if raw[0] == password:
+                    if raw[1]:
+                        employe = 1
+                        print("employe")
+                    else:
+                        employe = 0
+                        print("client")
+                else:
+                    print("Mauvais mot de passe\nVeuillez vous reconnecter")
+                    # retour menu
             else:
-                employe = 0
-        else:
-            print("Mauvais mot de passe")
-            #retour menu
-        if employe:
-            print("employe")
-            # menu employe
-        else:
-            print("client")
-            # menu client
+                print("Login inconnu\nVeuillez vous reconnecter")
+
     else:
         login = input("Quel login voulez-vous ?")
         # check si le login n'est pas déjà pris
