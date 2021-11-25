cba6683815a92e57462c47603398d14a330c3254
Alexandre Laval <alexandre.laval@etu.utc.fr>
2021-11-25 09:29:13 +0000

    Upload New File

---
+++ Rendu 5/insertData.sql
@@ -0,0 +1,110 @@
+INSERT into Client values ('0');
+INSERT into Client values ('1');
+INSERT into Client values ('2');
+INSERT into Client values ('3');
+INSERT into Client values ('4');
+INSERT into Client values ('5');
+INSERT into Client values ('6');
+INSERT into Client values ('7');
+INSERT into Client values ('8');
+INSERT into Client values ('9');
+INSERT into Client values ('10');
+INSERT into Client values ('11');
+insert into client values ('42');
+
+
+
+insert into occasionnel values('9');
+insert into occasionnel values('10');
+insert into occasionnel values('11');
+
+
+insert into abonne values('0','0','Dupont','Jacques','2000-03-26');
+insert into abonne values('1','13','Veil','Simone','1955-09-30');
+insert into abonne values('2','25','Dupond','Jacques','1972-01-28');
+insert into abonne values('3','15','Navre','Charlotte','1999-08-01');
+insert into abonne values('4','8','Poutire','Alice','1965-04-15');
+insert into abonne values('5','4','Janvier','Elise','1988-09-10');
+insert into abonne values('6','0','Vian','Boris','1967-07-07');
+insert into abonne values('7','30','Holmes','Sherlock','1994-10-18');
+insert into abonne values('8','17','Vivet','Lucie','1985-02-05');
+insert into abonne values('42','0','Biaux','Florestan','2000-03-26');
+
+INSERT into Vehicule values ('dsdsf111','vehicule simple','0');
+INSERT into Vehicule values ('11tger2','camion','8');
+INSERT into Vehicule values ('54d113','deux roues','2');
+INSERT into Vehicule values ('24fs5','vehicule simple','1');
+INSERT into Vehicule values ('32qger1','camion','5');
+INSERT into Vehicule values ('1qgr18','deux roues','6');
+INSERT into Vehicule values ('4eg51','vehicule simple','3');
+INSERT into Vehicule values ('8eg52','camion','4');
+INSERT into Vehicule values ('90rgop2','deux roues','7');
+INSERT into Vehicule values ('1gre19','vehicule simple','11');
+INSERT into Vehicule values ('415ger5','deux roues','10');
+
+INSERT into zone values ('Centre-ville','2','15');
+INSERT into zone values ('Industriel','1','5');
+INSERT into zone values ('Commercial','6','20');
+
+INSERT into parking values('1','Centre-ville','Mairie','1 place de la mairie,60200 Compiegne');
+INSERT into parking values('2','Centre-ville','Chateau','4 place du chateau, 60200 Compiegne');
+INSERT into parking values('3','Industriel','Usine','12 avenue du général Leclerc,60200 Compiegne');
+INSERT into parking values('4','Industriel','Centrale','1 Square du 8 mai 1945, 60200 Compiegne');
+INSERT into parking values('5','Commercial','Magasin','12 boulevard vivian,60200 Compiegne');
+INSERT into parking values('6','Commercial','Capitole','135 rue des Bateliers, 60200 Compiegne');
+
+Insert into place values('1','Centre-ville','1','camion','couverte');
+Insert into place values('1','Centre-ville','2','vehicule simple','plein air');
+Insert into place values('1','Centre-ville','3','camion','couverte');
+Insert into place values('2','Centre-ville','1','vehicule simple','plein air');
+Insert into place values('2','Centre-ville','2','vehicule simple','plein air');
+Insert into place values('2','Centre-ville','3','deux roues','couverte');
+Insert into place values('2','Centre-ville','4','vehicule simple','couverte');
+Insert into place values('3','Industriel','1','vehicule simple','plein air');
+Insert into place values('3','Industriel','2','vehicule simple','couverte');
+Insert into place values('3','Industriel','3','deux roues','couverte');
+Insert into place values('4','Industriel','1','vehicule simple','couverte');
+Insert into place values('4','Industriel','2','vehicule simple','plein air');
+Insert into place values('4','Industriel','3','vehicule simple','couverte');
+Insert into place values('4','Industriel','4','camion','couverte');
+Insert into place values('4','Industriel','5','deux roues','couverte');
+Insert into place values('5','Commercial','1','vehicule simple','plein air');
+Insert into place values('5','Commercial','2','vehicule simple','couverte');
+Insert into place values('6','Commercial','1','vehicule simple','plein air');
+Insert into place values('6','Commercial','2','camion','couverte');
+Insert into place values('6','Commercial','3','deux roues','plein air');
+
+
+Insert into Paiement values('1', '2', 'automate','1');
+Insert into Paiement values('2', '15', 'internet','0');
+Insert into Paiement values('3', '1', 'automate','4');
+Insert into Paiement values('4', '5', 'internet','5');
+Insert into Paiement values('5', '6', 'automate','8');
+Insert into Paiement values('6', '20', 'internet','6');
+Insert into Paiement values('7', '2', 'automate','11');
+Insert into Paiement values('8', '15', 'internet','9');
+Insert into Paiement values('9', '1', 'automate','3');
+Insert into Paiement values('10', '20', 'internet','2');
+Insert into Paiement values('11', '6', 'automate','10');
+
+
+
+insert into employe values ('1000359');
+insert into employe values ('2548720');
+insert into employe values ('7145986');
+insert into employe values ('2403210');
+insert into employe values ('7102503');
+
+insert into reservation values ('1','2021-11-15','2021-11-16','111','1','1','Centre-ville','2');
+
+
+insert into compte values ('0','randomail@utc.fr','0',NULL,'2');
+
+insert into Ticket values ('1','1','vehicule simple','2021-11-13','2021-11-14','Centre-ville','1');
+
+
+insert into abonnement values ('2','310','2021-11-26','2021-11-25','0');
+insert into abonnement values ('4','121','2023-10-08','2025-10-08','5');
+insert into abonnement values ('6','420','2021-11-30','2021-12-30','6');
+insert into abonnement values ('8','560','2021-12-05','2022-01-05','8');
+insert into abonnement values ('10','789','2022-01-18','2023-01-18','2');
