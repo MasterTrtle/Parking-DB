62c39228ade1b9e39eaed9578090f9b4b6bfb936
Alexandre Laval <alexandre.laval@etu.utc.fr>
2021-11-25 16:19:07 +0000

    Update generation.py

--- Rendu 5/generation.py
+++ Rendu 5/generation.py
@@ -1,7 +1,9 @@
 from faker import Faker
+
 import datetime
 import psycopg2
 import random
+from random_username.generate import generate_username
 
 
 def nouvelle_transaction(cur,montant,type_caisse,client):
@@ -15,12 +17,29 @@ def create_abo(cur, nom, prenom, naissance):
     id = cur.fetchone()[0]
     sql = "INSERT into Abonne values ('%s','0','%s','%s','%s');" % (id, nom, prenom, naissance)
     cur.execute(sql)
+    create_account_random(cur,'client',id)
+
 
 def insert_random_occasionel(cur,nombre):
     fake = Faker()
     for i in range(nombre):
-        sql = "INSERT into Client(id_client) values (DEFAULT);"
+        sql = "INSERT into Client(id_client) values (DEFAULT) RETURNING id_client;"
         cur.execute(sql)
+        id = cur.fetchone()[0]
+        sql = "Insert into Occasionnel values ('%s')"%id
+        cur.execute(sql)
+
+def create_account_random(cur,cible, id):
+    fake = Faker()
+    login =generate_username()[0]
+    mail = fake.email()
+    #sql = ""#verifier unicité mail
+    mdp = fake.password(10)
+    if cible =="employe":
+        sql = "INSERT INTO compte(login,mail,mdp,employe) values ('%s','%s','%s','%s');"%(login,mail,mdp,id)
+    else:
+        sql = "INSERT INTO compte(login,mail,mdp,abonne) values ('%s','%s','%s','%s');" % (login, mail, mdp, id)
+    cur.execute(sql)
 
 def insert_random_ticket(cur,id_transaction,id_client,type_vehicule):
     sql ="Insert into ticket values('%s',DEFAULT,'%s','%s','%s' );"%(id_transaction,type_vehicule,debut,fin,zone_parking,id_parking)
@@ -34,8 +53,9 @@ def create_zone(cur):
         cur.execute(sql)
 
 def create_parking(cur,zone,nom,adresse):
+
     sql="insert into parking values(default,'%s','%s','%s');"%(zone,nom,adresse)
-    cur.execute
+    cur.execute(sql)
 
 def generate_parking(cur,nombre):
     fake = Faker()
@@ -56,9 +76,21 @@ def insert_random_abo(cur, nombre):
         naissance = fake.date_between_dates(debut, fin)
         create_abo(cur, nom, prenom, naissance)
 
+def create_employe(cur,numSecu):
+    sql = "INSERT into employe values (DEFAULT, '%s') RETURNING id_employe;"%numSecu
+    cur.execute(sql)
+    id = cur.fetchone()[0]
+    create_account_random(cur,'employe',id)
+
+def insert_random_employe(cur,nombre):
+    fake = Faker()
+    for i in range(nombre):
+        numSecu=random.getrandbits(10)
+        create_employe(cur,numSecu)
+
 def generate_vehicule(cur,client):
     fake = Faker()
-    for i in range(random.randint(0,10)):
+    for i in range(random.randint(0,5)):
         inmat=str(fake.license_plate())
         type_vehicule = ['deux roues', 'camion', 'vehicule simple'][random.randint(0,2)]
         sql = "insert into vehicule values('%s','%s',%s);"%(inmat,type_vehicule,client)
@@ -66,15 +98,13 @@ def generate_vehicule(cur,client):
 
 
 def generate_vehicule_all_clients(cur):
-    sql= "select * from Client"
+    sql= "select id_client from Client;"
     cur.execute(sql)
-    raw=cur.fetchone()
-    while raw:
-        generate_vehicule(cur, raw[0])
-        try:
-            raw = cur.fetchone()
-        except:
-            break
+    raw=cur.fetchall()
+    for client in raw:
+        generate_vehicule(cur, client[0])
+
+
 
 def generate_all_account(cur):
     return
@@ -85,4 +115,5 @@ def remplir_bdd(cur):
     generate_parking(cur, 15)
     insert_random_abo(cur,20)
     insert_random_occasionel(cur,20)
+    insert_random_employe(cur,5)
     generate_vehicule_all_clients(cur)
