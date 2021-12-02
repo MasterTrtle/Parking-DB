162c74d3bc8ad21ef99f5e18b48531ba34daf502
Alexandre Laval <alexandre.laval@etu.utc.fr>
2021-12-02 10:01:48 +0000

    Update menu.py

--- Rendu 5/menu.py
+++ Rendu 5/menu.py
@@ -4,42 +4,39 @@ import reservation
 def menu_employe(cur,conn,login):
     print("---Bienvenue dans l'espace employé---")
     choix=0
-    loop='true'
-    while (loop=='true'):
+    loop = True
+    while loop:
         print("Que voulez-vous faire?")
         print("[1] - Changer un parking de zone")
         print("[2] - Changer le tarif d'une zone")
         print("[3] - Terminer")
         choix=input("Entrez votre choix : ")
         if choix=='1' :
-            loop='false'
             zone.changer_zone_parking(cur,conn)
-            loop='true'
         elif choix=='2' :
-            loop='false'
             zone.changer_tarif_zone(cur,conn)
-            loop='true'
         elif choix=='3' :
+            loop=False
             return
         
 def menu_client(cur,conn,login):
     print("\n---Bienvenue dans votre espace client---")
     choix=0
-    loop='true'
-    while (loop=='true'):
+    loop = True
+    while loop:
         print("Que voulez-vous faire?")
         print("[1] - Reserver une place")
-        print("[2] - ")
-        print("[3] - Terminer")
+        print("[2] - Consulter|Modifier votre liste de véhicules")
+        print("[3] - Consulter|Modifier vos abonnement")
+        print("[4] - Consulter|Modifier vos réservations")
+        print("[5] - Terminer")
         choix=input("Entrez votre choix : ")
         if choix=='1' :
-            loop='false'
             reservation.reserver_place(cur,login)
-            loop='true'
-        elif choix=='2' :
-            loop='false'
-            #fonction
-            loop='true'
-        elif choix=='3' :
+        elif choix=='2':
             return
+        elif choix=='4' :
+            return
+        elif choix == '5':
+            loop = False
     
