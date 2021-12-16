4f1e17564cb58bb9050d73cace0d7c07a3ae3a95
ezglinic <emma.zglinicki@etu.utc.fr>
2021-12-16 11:13:57 +0100

    Ajout fonction stat

--- Rendu5b/menu.py
+++ Rendu5b/menu.py
@@ -9,7 +9,9 @@ def menu_employe(cur,conn,login):
         print("Que voulez-vous faire?")
         print("[1] - Changer un parking de zone")
         print("[2] - Changer le tarif d'une zone")
-        print("[3] - Terminer")
+        print("[3] - Voir le nombre d'abonnements par zone")
+        print("[4] - Voir le nombre de parkings par zone")
+        print("[5] - Terminer")
         choix=input("Entrez votre choix : ")
         if choix=='1' :
             loop='false'
@@ -20,8 +22,16 @@ def menu_employe(cur,conn,login):
             zone.changer_tarif_zone(cur,conn)
             loop='true'
         elif choix=='3' :
+            loop='false'
+            statistiques.Nombre_Abonnements_Zone(cur)
+            loop='true'
+        elif choix=='4' :
+            loop='false'
+            statistiques.Nombre_Parkings_Zone(cur)
+            loop='true'
+        elif choix=='5' :
             return
-        
+
 def menu_client(cur,conn,login):
     print("\n---Bienvenue dans votre espace client---")
     choix=0
@@ -29,8 +39,9 @@ def menu_client(cur,conn,login):
     while (loop=='true'):
         print("Que voulez-vous faire?")
         print("[1] - Reserver une place ")
-        print("[2] - ")
-        print("[3] - Terminer")
+        print("[2] - Consulter son solde de fidélité")
+        print("[3] - ")
+        print("[4] - Terminer")
         choix=input("Entrez votre choix : ")
         if choix=='1' :
             loop='false'
@@ -38,8 +49,11 @@ def menu_client(cur,conn,login):
             loop='true'
         elif choix=='2' :
             loop='false'
-            #fonction
+            statistiques.Solde_Fidelite(cur, idabonne)
             loop='true'
         elif choix=='3' :
+            loop='false'
+            #fonction
+            loop='true'
+        elif choix=='4' :
             return
-    
